744fce26ac49f8458e416130de35ae0d669608ee
Lautaro261 <nahuelygoofy@gmail.com>
2023-04-19 00:14:39 -0300

    segundo commit de ramaLautaro1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
      <h1>HOLA MUNDO. PRUEBA DE RAMA 1</h1>
+     <p>segunda prueba de contribucion</p>
+     <button>Click</button>
     </div>
   );
 }
